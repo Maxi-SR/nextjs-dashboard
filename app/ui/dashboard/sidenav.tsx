269bb3485f94f85f9079ca8d04b36b918d481ca1
Maxi-SR <schmidtmaxi46@gmail.com>
2023-12-17 23:21:58 -0300

    change to billsplitpro

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
+import Image from 'next/image';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { signOut } from '@/auth';
 
@@ -12,7 +12,12 @@ export default function SideNav() {
         href="/"
       >
         <div className="w-32 text-white md:w-40">
-          <AcmeLogo />
+        <Image
+              src="/logo_stats.png"
+              width={200}
+              height={50}
+              alt="logo"
+            />
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
